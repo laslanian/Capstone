4d715871b28a10f979d5af5ece7a1387fce74b5c
karlo dela merced <simplykarlo10@hotmail.com>
2016-10-04 18:34:12 -0400

    10.4.2016 viewbag error student reg

--- CapstoneProject/CapstoneProject/Models/Services/UserAccountService.cs
+++ CapstoneProject/CapstoneProject/Models/Services/UserAccountService.cs
@@ -39,67 +39,61 @@ namespace CapstoneProject.Models.Services
             {
                 s.Program = pms.GetProgram(stUser.ProgramId);
             }
-            using (UserAccountService uas = new UserAccountService())
+            s.FirstName = stUser.FirstName;
+            s.LastName = stUser.LastName;
+            s.PhoneNumber = stUser.PhoneNumber;
+            s.Email = stUser.Email;
+            s.Username = stUser.Username;
+            s.Password = stUser.Password;
+            s.StudentNumber = Convert.ToInt32(stUser.StudentNumber);
+            s.Title = "Student";
+
+            AesEncrpyt en = new AesEncrpyt();
+            s.Username = en.Encrypt(stUser.Username);
+            s.Password = en.Encrypt(stUser.Password);
+
+            //1 - success
+            //0 - failure 
+            if (!_users.isExistingUsername(s.Username))
             {
-                s.FirstName = stUser.FirstName;
-                s.LastName = stUser.LastName;
-                s.PhoneNumber = stUser.PhoneNumber;
-                s.Email = stUser.Email;
-                s.Username = stUser.Username;
-                s.Password = stUser.Password;
-                s.StudentNumber = Convert.ToInt32(stUser.StudentNumber);
-                s.Title = "Student";
-
-                AesEncrpyt en = new AesEncrpyt();
-                s.Username = en.Encrypt(stUser.Username);
-                s.Password = en.Encrypt(stUser.Password);
-
-                //1 - success
-                //0 - failure 
-                if (!_users.isExistingUsername(s.Username))
-                {
-                    _users.InsertUser(s);
-                    _users.Save();
-                    return 1;
-                }
-                else
-                {
-                    return 0;
-                }
+                _users.InsertUser(s);
+                _users.Save();
+                return 1;
+            }
+            else
+            {
+                return 0;
             }
         }
         public int RegisterClient(ClientUser client)
         {
             Client s = new Client();
 
-            using (UserAccountService uas = new UserAccountService())
+            s.FirstName = client.FirstName;
+            s.LastName = client.LastName;
+            s.PhoneNumber = client.PhoneNumber;
+            s.Email = client.Email;
+            s.Username = client.Username;
+            s.Password = client.Password;
+            s.CompanyName = client.CompanyName;
+            s.CompanyAddress = client.CompanyAddress;
+            s.CompanyDescription = client.CompanyDesc;
+
+            s.Title = "Client";
+
+            AesEncrpyt en = new AesEncrpyt();
+            s.Username = en.Encrypt(client.Username);
+            s.Password = en.Encrypt(client.Password);
+
+            if (!_users.isExistingUsername(s.Username))
             {
-                s.FirstName = client.FirstName;
-                s.LastName = client.LastName;
-                s.PhoneNumber = client.PhoneNumber;
-                s.Email = client.Email;
-                s.Username = client.Username;
-                s.Password = client.Password;
-                s.CompanyName = client.CompanyName;
-                s.CompanyAddress = client.CompanyAddress;
-                s.CompanyDescription = client.CompanyDesc;
-
-                s.Title = "Client";
-
-                AesEncrpyt en = new AesEncrpyt();
-                s.Username = en.Encrypt(client.Username);
-                s.Password = en.Encrypt(client.Password);
-
-                if (!_users.isExistingUsername(s.Username))
-                {
-                    _users.InsertUser(s);
-                    _users.Save();
-                    return 1;
-                }
-                else
-                {
-                    return 0;
-                }
+                _users.InsertUser(s);
+                _users.Save();
+                return 1;
+            }
+            else
+            {
+                return 0;
             }
         }
 
