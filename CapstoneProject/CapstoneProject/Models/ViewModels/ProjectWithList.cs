576d84a4906738f7f022cdea750c3a4e26b426fa
Leo <aslanian@sheridancollege.com>
2016-10-31 17:06:43 -0400

    moved sessionstate to correct webconfig

--- CapstoneProject/CapstoneProject/Models/ViewModels/ProjectWithList.cs
+++ CapstoneProject/CapstoneProject/Models/ViewModels/ProjectWithList.cs
@@ -17,7 +17,7 @@ namespace CapstoneProject.Models.ViewModels
             States.Add(new SelectListItem { Text = ProjectState.Pending, Value = ProjectState.Pending });
             States.Add(new SelectListItem { Text = ProjectState.Approved, Value = ProjectState.Approved });
             States.Add(new SelectListItem { Text = ProjectState.Rejected, Value = ProjectState.Rejected });
-            States.Add(new SelectListItem { Text = ProjectState.Assinged, Value = ProjectState.Assinged });
+            States.Add(new SelectListItem { Text = ProjectState.Assigned, Value = ProjectState.Assigned });
         }
         public string SelectedItem { get; set; }
         public List<Project> Projects { get; set; }
--- CapstoneProject/CapstoneProject/Utility/ProjectState.cs
+++ CapstoneProject/CapstoneProject/Utility/ProjectState.cs
@@ -10,7 +10,7 @@ namespace CapstoneProject.Utility
         public static string Pending { get { return "Pending"; } }
         public static string Approved { get { return "Approved"; } }
         public static string Rejected { get { return "Rejected"; } }
-        public static string Assinged { get { return "Assigned"; } }
+        public static string Assigned { get { return "Assigned"; } }
         public static string All { get { return "All"; } }
     }
 }
