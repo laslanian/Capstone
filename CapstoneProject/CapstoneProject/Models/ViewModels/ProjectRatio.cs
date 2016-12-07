8cef50c360f67b368946cd2f331a1dd347cf21a2
karlo dela merced <simplykarlo10@hotmail.com>
2016-12-06 23:33:23 -0500

    fix

---
+++ CapstoneProject/CapstoneProject/Models/ViewModels/ProjectRatio.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CapstoneProject.Models.ViewModels
+{
+    public class ProjectRatio
+    {
+        public int Approved { get; set; }
+        public int Rejected { get; set; }
+        public int Completed { get; set; }
+        public int Pending { get; set; }
+        public int Assigned { get; set; }
+
+        public int Total { get; set; }
+
+        public ProjectRatio()
+        {
+
+            Approved = 0;
+            Rejected = 0;
+            Completed = 0;
+            Pending = 0;
+            Assigned = 0;
+        }
+
+        public int GetTotal()
+        {
+            return Approved + Rejected + Completed + Pending + Assigned;
+        }
+    }
+}
