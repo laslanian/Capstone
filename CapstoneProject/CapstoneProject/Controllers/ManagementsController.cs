c011988f1cfa46da39d2c14e7f9b8618670fdd4f
Leo <aslanian@sheridancollege.com>
2016-12-07 08:51:10 -0500

    user type chart

--- CapstoneProject/CapstoneProject/Controllers/ManagementsController.cs
+++ CapstoneProject/CapstoneProject/Controllers/ManagementsController.cs
@@ -59,5 +59,39 @@ namespace CapstoneProject.Controllers
             gr.NotAssignedPct = gr.GetNotAssignedPercent();
             return View(gr);
         }
+        [NoDirectAccess]
+        public ActionResult UsersReports()
+        {
+            List<User> Users = _uas.GetUsers();
+            UserRatio ur = new UserRatio();
+
+            ur.Students = Users.FindAll(u => u.Type == AccountType.Student).Count();
+            ur.Clients = Users.FindAll(u => u.Type == AccountType.Client).Count();
+            ur.Admins = Users.FindAll(u => u.Type == AccountType.Admin).Count();
+            ur.Advisors = Users.FindAll(u => u.Type == AccountType.Coop_Advisor).Count();
+            ur.Management = Users.FindAll(u => u.Type == AccountType.Management).Count();
+
+            ur.Total = Users.Count();
+
+            ur.ClientPct = ur.GetClientRatio();
+            ur.AdminPct = ur.GetAdminRatio();
+            ur.AdvisorPct = ur.GetAdvRatio();
+            ur.MngPct = ur.GetMngRatio();
+            ur.StudentPct = ur.GetStudentRatio();
+
+            return View(ur);
+        }
+        public ActionResult UserChart(int students, int clients, int admins, int advisors, int mng)
+        {
+            UserRatio ur = new UserRatio();
+            ur.Students = students;
+            ur.Clients = clients;
+            ur.Admins = admins;
+            ur.Advisors = advisors;
+            ur.Management = mng;
+
+
+            return View(ur);
+        }
     }
 }
---
+++ CapstoneProject/CapstoneProject/Models/ViewModels/UserRatio.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CapstoneProject.Models.ViewModels
+{
+    public class UserRatio
+    {
+        public double Students { get; set; }
+        public double Admins { get; set; }
+        public double Management { get; set; }
+        public double Clients { get; set; }
+        public double Advisors { get; set; }
+        public double Total { get; set; }
+
+        public double StudentPct { get; set; }
+        public double AdminPct { get; set; }
+        public double MngPct { get; set; }
+        public double ClientPct { get; set; }
+        public double AdvisorPct { get; set; }
+
+        public UserRatio() {
+            this.Students = 0;
+            this.Admins = 0;
+            this.Management = 0;
+            this.Clients = 0;
+            this.Advisors = 0;
+            this.Total = 0;
+        }
+
+        public double GetStudentRatio()
+        {
+            double value = (Students / Total) * 100;
+            return Math.Round(value, 2);
+        }
+        public double GetClientRatio()
+        {
+            double value = (Clients / Total) * 100;
+            return Math.Round(value, 2);
+        }
+        public double GetAdminRatio()
+        {
+            double value = (Admins / Total) * 100;
+            return Math.Round(value, 2);
+        }
+        public double GetMngRatio()
+        {
+            double value = (Management / Total) * 100;
+            return Math.Round(value, 2);
+        }
+        public double GetAdvRatio()
+        {
+            double value = (Advisors / Total) * 100;
+            return Math.Round(value, 2);
+        }
+    }
+}
